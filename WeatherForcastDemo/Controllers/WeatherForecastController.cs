efe3bb2390abd50f535cba5882335f489be60a58
刘群 <517067180@qq.com>
2023-01-08 18:54:51 +0800

    学习POST请求

--- WeatherForcastDemo/Controllers/WeatherForecastController.cs
+++ WeatherForcastDemo/Controllers/WeatherForecastController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace WeatherForcastDemo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[Action]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -29,6 +30,29 @@ namespace WeatherForcastDemo.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost]
+
+        public ActionResult<MyCalcRequestDto> PostMyData([FromBody]MyCalcRequestDto dto)
+        {
+            long sum = dto.A + dto.B;
+            if (sum == 0)
+            {
+                return NotFound("a+b=0被禁止");
+            }
+            return Ok(dto);
+        }
+
+        public class MyCalcRequestDto
+        {
+            [Required]
+            public int A { get; set; }
+            [Required]
+            public int B { get; set; }
+            [Required(AllowEmptyStrings = true, ErrorMessage = "name字段是必填项")]
+            [MinLength(4,ErrorMessage = "name字段长度要求至少4字节")]
+            public string? Name { get; set; }
         }
     }
 }
